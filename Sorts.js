99869ad8b075af96f8ce454fad1050fe77ceebb2
kailahk <kailahkorsh@me.com>
2023-02-26 11:23:38 -0800

    scommon sorting methods and runtimes

---
+++ Sorts.js
@@ -0,0 +1,25 @@
+// COMMON SORTING METHODS
+
+// Comparison sorts: compares two values and decides which to put first
+// Distribution sorts: uses some property of the items to decide where they fit
+// stability: preserves the initial order of a collection
+
+// Type of sort - Best Case, Avg Case, Worst Case, Space, Stability, Sorting Method
+
+// Bubble sort - Ω(N) , Θ(N^2) , O(N^2)	, O(1) , Stable , Comparison
+
+
+
+// Insertion sort - Ω(N) , Θ(N^2) , O(N^2) , O(1) , Stable , Comparison
+
+
+// Bucket sort - Ω(N+K) , Θ(N+K) , O(N^2) , O(N+K) , Stable , Distribution
+
+
+// Radix sort - Ω(NK) , Θ(NK) , O(NK) , O(N+K) , Stable , Distribution
+
+
+// Merge sort - Ω(log(N)) , Θ(log(N)) , O(log(N)) , O(N) , Stable , Comparison
+
+
+// Quick sort - Ω(log(N))	Θ(log(N)) , O(N^2) , O(log(N)) , Unstable , Comparison
